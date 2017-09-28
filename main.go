b89207a73241a19b44846b4ce4ba858b77be201f
PORTHOS Anthony <anthony.porthos@orange.com>
2017-09-28 11:28:21 +0200

    init

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"net/http"
 	"log"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 	log.Printf("Running server...")
-	err := http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
